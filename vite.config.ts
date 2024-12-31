b23a51b28f1f12200fe8d1fa401f61f87a4faf3c
Caio Galdino <caio.vn.gald@gmail.com>
2024-12-31 16:04:55 -0300

    Ajuste nas Configurações do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SiteProject"
+  base: "/SiteProject",
+  build: {
+    assetsInlineLimit: 0,
+  },
 })
